8f4188998edb9f35fc653fbc960967882261e92e
Christopher Su <christophersu9@gmail.com>
2013-03-01 09:37:08 +0800

    variable organization and fixes

--- 2012-2013 Season/Headers/CSGlobalVariables.h
+++ 2012-2013 Season/Headers/CSGlobalVariables.h
@@ -10,25 +10,25 @@
 #define JOYSTICK_LOWER_THRESHOLD -15
 
 #define ARM_TO_TOP_REVOLUTIONS 17
-#define ARM_TO_MIDDLE_REVOLUTIONS 8
+#define ARM_TO_MIDDLE_REVOLUTIONS 8 //guessed, hasn't been tested yet
 
-//autonomous
+//autonomous, kept in GlobalVariables so that the Methods header may be use them
 int autonomous10Mdelay = 10;
 int irSeekingDelay = 50;
 int irStrafeCount = 1;
 int autonomousWheelPower = 90;
 
-//legacy
-int joystickUpperThreshold = 15;
-int joystickLowerThreshold = -15;
+//replaced by JOYSTICK_UPPER_THRESHOLD and JOYSTICK_LOWER_THRESHOLD
+//int joystickUpperThreshold = 15;
+//int joystickLowerThreshold = -15;
 
 //tMotor simultaneousTwoMotorEncodeMotorOne = motor12;
 //tMotor simultaneousTwoMotorEncodeMotorTwo = motor22;
 //int simultaneousTwoMotorEncodePower = 75;
 //float simultaneousTwoMotorEncodeRevolutions = 1;
-float armToTopRevolutions = 17;
+//float armToTopRevolutions = 17; //replaced by ARM_TO_TOP_REVOLUTIONS
 int touchMuxerValue;
-bool touchMuxer1, touchMuxer2, touchMuxer3, touchMuxer4;
+short touchMuxer1, touchMuxer2, touchMuxer3, touchMuxer4;
 //int autonomousIRseekerTarget = 4;
 //int controllerAMultiplier = 1;
 //float motorSpeedFixMultiplier11 = 1;
